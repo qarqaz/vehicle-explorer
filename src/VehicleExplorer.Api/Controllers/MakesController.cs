215aed6d252a483dda12e1e4de1452698b4f0c21
qarqaz <theqarqazkhaled@gmail.com>
2026-09-25 18:56:10 +0300

    add vehicle makes endpoint

---
+++ src/VehicleExplorer.Api/Controllers/MakesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VehicleExplorer.Api.Models.Responses;
+using VehicleExplorer.Api.Services;
+
+namespace VehicleExplorer.Api.Controllers
+{
+    [Route("api/makes")]
+    [ApiController]
+    public class MakesController : ControllerBase
+    {
+        private readonly IVehicleService _vehicleService;
+
+        public MakesController(IVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IReadOnlyList<MakeResponse>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IReadOnlyList<MakeResponse>>> GetMakes(CancellationToken cancellationToken)
+        {
+            var makes = await _vehicleService.GetMakesAsync(cancellationToken);
+
+            var response = makes.Select(make => new MakeResponse
+            {
+                Id = make.MakeId,
+                Name = make.MakeName
+            })
+                .OrderBy(make => make.Name)
+                .ToArray();
+
+            return Ok(response);
+        }
+    }
+}
---
+++ src/VehicleExplorer.Api/Models/Responses/MakeResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleExplorer.Api.Models.Responses
+{
+    public class MakeResponse
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; } = string.Empty;
+    }
+}
